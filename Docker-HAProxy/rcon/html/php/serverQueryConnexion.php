938c46c6c5a8696964eecdc027c350287619eb14
Tihi <50585160+zfiel@users.noreply.github.com>
2020-03-24 20:30:11 +0100

    Update serverQueryConnexion.php

--- Docker-HAProxy/rcon/html/php/serverQueryConnexion.php
+++ Docker-HAProxy/rcon/html/php/serverQueryConnexion.php
@@ -1,7 +1,7 @@
 <?php
   require_once('query.php');
 
-$server = new Query(getenv('BUNGEECORD_IP'),36543);
+$server = new Query(getenv('MINECRAFT_2_IP'),25562);
 if ($server->connect())
 {
   $info = $server->get_info();
