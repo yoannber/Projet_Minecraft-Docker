ef2cbfac279fa56fad9c522bc60cb21dcb9bb1d9
Tihi <50585160+zfiel@users.noreply.github.com>
2020-03-24 19:48:30 +0100

    Update index.php

--- Docker-HAProxy-BungeeCord/rcon/html/html/index.php
+++ Docker-HAProxy-BungeeCord/rcon/html/html/index.php
@@ -2,11 +2,8 @@
         use xPaw\MinecraftPing;
         use xPaw\MinecraftPingException;
 
-	# $gg = $_ENV['DIR']."_minecraft_1";
-	       $gg = getenv('DIR');
-	$ggMoc = $gg."_minecraft_1";
         // Edit this ->
-        define( 'MQ_SERVER_ADDR', $ggMoc );
+        define( 'MQ_SERVER_ADDR', getenv('MINECRAFT_1_IP') );
         define( 'MQ_SERVER_PORT', 25566 );
         define( 'MQ_TIMEOUT', 1 );
         // Edit this <-
