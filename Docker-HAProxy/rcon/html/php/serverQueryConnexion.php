c39e246c158cf3768af313a80cd9b66ba44c4bc2
thierry <thierry@debian.yoannbernard.com>
2020-03-24 19:43:26 +0100

    gg

--- Docker-HAProxy/rcon/html/php/serverQueryConnexion.php
+++ Docker-HAProxy/rcon/html/php/serverQueryConnexion.php
@@ -1,7 +1,7 @@
 <?php
   require_once('query.php');
 
-$server = new Query(getenv('MINECRAFT_1_IP'),25566);
+$server = new Query(getenv('BUNGEECORD_IP'),36543);
 if ($server->connect())
 {
   $info = $server->get_info();
